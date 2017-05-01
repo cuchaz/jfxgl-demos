4971fe6c0edee54c7ba3d37677451213a6be7d85
jeff <devnull@localhost>
2017-04-30 23:45:25 -0400

    improve shader compatibility

--- resources/cuchaz/jfxgl/demo/triangle.vertex.glsl
+++ resources/cuchaz/jfxgl/demo/triangle.vertex.glsl
@@ -7,7 +7,7 @@
  * 
  * See LICENSE.txt in the project root folder for the full license.
  *************************************************************************/
-#version 150 core
+#version 130
 
 uniform mat4 model;
 uniform mat4 view;
