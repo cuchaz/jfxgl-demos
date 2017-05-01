7bc15212034857286555731ed7475e29d7a6bf4f
jeff <jeff@WINBEAST>
2017-04-30 20:53:33 -0400

    update build script to be more agnostic of OpenJDK version

--- build/def/cuchaz/jfxgl/demo/Build.java
+++ build/def/cuchaz/jfxgl/demo/Build.java
@@ -30,7 +30,7 @@ public class Build extends JkJavaBuild {
 		// tell the eclipse plugin to use the special JDK without JavaFX
 		// NOTE: you should create a JRE in the  eclipse workspace needs to have a JRE with this name!
 		JkBuildPluginEclipse eclipse = new JkBuildPluginEclipse();
-		eclipse.setStandardJREContainer("openjdk-8u131-noFX");
+		eclipse.setStandardJREContainer("openjdk-noFX");
 		plugins.configure(eclipse);
 	}
 	
