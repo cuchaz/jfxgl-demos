11c5912064720fa5556cedee0939cc6c1db0cf48
jeff <devnull@localhost>
2017-06-19 19:45:43 -0400

    add some more widgets to popups demo

--- src/cuchaz/jfxgl/demo/popups/MainController.java
+++ src/cuchaz/jfxgl/demo/popups/MainController.java
@@ -1,17 +1,30 @@
 package cuchaz.jfxgl.demo.popups;
 
+
 import cuchaz.jfxgl.CalledByEventsThread;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Dialog;
 
 public class MainController {
 
 	@FXML private ChoiceBox<String> choice;
+	@FXML private Button dialogButton;
 	
 	@FXML
 	@CalledByEventsThread
 	public void initialize() {
+		
 		choice.getItems().setAll("Cheese", "Beer", "Cake");
 		choice.getSelectionModel().select(0);
+		
+		dialogButton.setOnAction((e) -> {
+			Dialog<?> dialog = new Dialog<>();
+			dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+			dialog.setContentText("Hello World!");
+			dialog.show();
+		});
 	}
 }
